82d076c06d503347aac93cff428efe40cc919a5e
Satyam.singh <Satyamkumarsingh759@gmail.com>
2025-11-15 19:17:33 +0530

    Java code pushed

--- src/main/java/org/collections/comparatorexample/ComparatorExample.java
+++ src/main/java/org/collections/comparatorexample/ComparatorExample.java
@@ -1,10 +1,21 @@
 package org.collections.comparatorexample;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class ComparatorExample {
     public static void main(String[] args) {
-
+           Integer[] a = {3,2,5,6,71,11,23};
+           Arrays.sort(a);// default ascending sorting order
+           System.out.println("Elements are in ascending order:");
+           for(int x:a){
+               System.out.println(x);
+           }
+           Arrays.sort(a,(x,y)-> y-x);// Using comparator
+           System.out.println("Elements are in descending order:");
+           for(int y:a){
+               System.out.println(y);
+           }
     }
 }
